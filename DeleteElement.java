95094c4f7a0f1ad9c633ef7a7ee8f8cbac89b6c4
tmtri3102 <tmtri310251@gmail.com>
2024-10-11 16:29:04 +0700

    delete element in array

--- DeleteElement.java
+++ DeleteElement.java
@@ -7,14 +7,30 @@ public class DeleteElement {
         int[] arr = {1,4,5,7,8,9,0};
         System.out.print("Find a number to delete: ");
         int num = scanner.nextInt();
+        boolean found = false;
+        int index_del = -1;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == num) {
-                for (int j = i; j < arr.length-1; j++) {
-                    arr[j] = arr[j+1];
+        while (!found) {
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == num) {
+                    index_del = i;
+                    found = true;
+                    break;
                 }
-                System.out.println(Arrays.toString(arr));
+            }
+            if (!found) {
+                System.out.print("Find a number to delete: ");
+                num = scanner.nextInt();
             }
         }
+
+
+
+        for (int j = index_del; j < arr.length-1; j++) {
+            arr[j] = arr[j+1];
+        }
+        System.out.println(Arrays.toString(arr));
+
+
     }
 }
